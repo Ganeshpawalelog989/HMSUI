0d035f4f6ccf1f88a6430af756edd8c86077e13a
MeharshGoh <Meharsh.Gohadkar@citiustech.com>
2021-07-27 15:14:23 +0530

    Small Changes

--- src/app/modules/change-password/change-password.component.ts
+++ src/app/modules/change-password/change-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { parse } from 'date-fns';
 import { Login } from 'src/app/Model/Login';
 import { AuthenticationService } from 'src/app/Service/AuthService';
 
@@ -13,29 +14,34 @@ export class ChangePasswordComponent implements OnInit {
     hide: boolean = false;
     loading = false;
     submitted = false;
+    
   //  pwdpattern='^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])([a-zA-Z0-9@$!%*?&]{8,})$';
     
   constructor(private fb: FormBuilder,
     private passServ : AuthenticationService,
     private router : Router) { }
 
+ // @Input() email : string;
+
   ngOnInit(): void {
+    
   }
 
   
   changepassForm: FormGroup = this.fb.group({
     email : [''],
+  defaultPass : ['',Validators.required],
    // password: ['', Validators.required,  Validators.minLength(8)],
     newpassword: ['', Validators.required],
     confirmnewpassword: ['', Validators.required]
-  })
+  },
   
-  passObj : Login = new Login(this.f.email.value,this.f.newpassword.value,
-    this.f.confirmnewpassword.value)
-  // {
-  //   validators:this.MustMatch('newpassword','confirmnewpassword')
-  // }\
-// )
+  // passObj : Login = new Login(this.f.email.value,this.f.newpassword.value,
+  //   this.f.confirmnewpassword.value)
+  {
+    validators:this.MustMatch('newpassword','confirmnewpassword')
+  }
+)
   get f() { return this.changepassForm.controls; }
 
   MustMatch(controlName:string,matchingControlName:string){
